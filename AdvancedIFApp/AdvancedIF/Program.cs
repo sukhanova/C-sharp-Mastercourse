84f42321b62d6252fa6b5edb4395900bd85afe2b
sukhanova <verusia.s@gmail.com>
2022-10-26 18:53:15 -0500

    Added If Statements coursework)

---
+++ AdvancedIFApp/AdvancedIF/Program.cs
@@ -0,0 +1,88 @@
+﻿
+/*
+ Write code to accept the height of a person in centimeter
+and categorize the person according to their height.
+ 
+ */
+//Console.Write("What is your height: ");
+//float userHeight = Convert.ToInt32(Console.ReadLine());
+
+//if (userHeight < 150.0)
+//{
+//    Console.WriteLine("You are a dwarf");
+//}
+//else if (userHeight >= 150.0 && userHeight <= 165.0)
+//{
+//    Console.WriteLine("You have average height");
+//}
+//else if (userHeight >= 165.0 && userHeight <= 195.0)
+//{
+//    Console.WriteLine("You are tall");
+//}
+//else
+//{
+//    Console.WriteLine("Abnormal height");
+//}
+
+//Console.WriteLine("End of program");
+
+/*
+ Write a program to read temperature in centigrade
+and display a suitable message according to temperature state below:
+Temp < 0 then Freezing weather
+Temp 0-10 then Very Cold weather
+Temp 10-20 then Cold weather
+Temp 20-30 then Normal in Temp
+Temp 30-40 then Its Hot
+Temp >=40 then Its Very Hot
+Test Data :
+ */
+
+//Console.Write("What is the temperature on thermometer: ");
+//int tempInput = Convert.ToInt32(Console.ReadLine());
+
+//if (tempInput < 0)
+//{
+//    Console.WriteLine("Freezing weather");
+//}
+//else if (tempInput <= 10)
+//{
+//    Console.WriteLine("Very Cold weather");
+//}
+//else if (tempInput <= 20)
+//{
+//    Console.WriteLine("Cold weather");
+//}
+//else if (tempInput <= 30)
+//{
+//    Console.WriteLine("It's Normal");
+//}
+//else if (tempInput <= 40)
+//{
+//    Console.WriteLine("It's Hot");
+//}
+//else if (tempInput >= 40)
+//{
+//    Console.WriteLine("It's very Hot");
+//}
+
+//Console.WriteLine("End of program");
+
+//Identify if you are eligible to register for service
+
+Console.Write("Are you male: ");
+string userSex = Console.ReadLine();
+
+Console.Write("How old are you? ");
+int userAge = Convert.ToInt32(Console.ReadLine());
+
+if ((userSex.ToLower() == "yes") && ((userAge >= 18) && (userAge <= 26)))
+{
+    Console.WriteLine("You are eligible to register for service.");
+}
+else
+{
+    Console.WriteLine("Unfortunately, You are not eligible to register for service!");
+}
+
+Console.WriteLine("End of program");
---
+++ BasicIFApp/BasicIF/Program.cs
@@ -0,0 +1,82 @@
+﻿
+
+// Conditional will help to have branching logic
+
+//bool isComplete = false;
+
+
+//if (isComplete)
+//{
+//    Console.WriteLine("Great! Move to the next chapter");
+//}
+//else
+//{
+//    Console.WriteLine("Please complete current chapter first");
+//}
+
+
+
+// you can have multiple if statements
+/*
+ If statements are executed independent of one another; each one will run.
+Else if statements only execute if the previous ifs fail.
+ */
+
+//Console.Write("What is your first name: ");
+//string? firstName = Console.ReadLine();
+
+//if (firstName.ToLower() == "oliver")
+//{
+//    Console.WriteLine("Welcome back, Oliver!");
+//}
+//if (firstName.ToUpper() == "JANE")
+//{
+//    Console.WriteLine("Oh, hello, Jane!");
+//}
+//else
+//{
+//    Console.WriteLine($"Hello, {firstName}");
+//}
+//Console.WriteLine("End of program");
+
+
+// Write code to accept two integers and check whether they are equal or not
+//using System.ComponentModel;
+
+//Console.WriteLine("----------");
+//Console.WriteLine("Check whether two integers are equal or not");
+//Console.Write("Type in first number: ");
+//int num1 = Convert.ToInt32(Console.ReadLine());
+
+//Console.Write("Type in second number: ");
+//int num2 = Convert.ToInt32(Console.ReadLine());
+
+//if (num1 == num2)
+//{
+//    Console.WriteLine($"{num1} and {num2} are equal");
+//}
+//else
+//{
+//    Console.WriteLine($"{num1} and {num2} are not equal");
+//}
+
+//Console.WriteLine("End of program");
+
+
+/* Write code to read the age of a candidate and determine whether it is eligible for
+casting his/her own vote */
+
+Console.WriteLine("----------");
+Console.WriteLine("Check if you are eligible to vote");
+Console.Write("How old are you: ");
+int age = Convert.ToInt32(Console.ReadLine());
+if (age >= 21)
+{
+    Console.WriteLine("Congratulation! You are eligible for casting your vote.");
+}
+else
+{
+    Console.WriteLine("Unfortunately you are too young to vote.");
+}
+
+Console.WriteLine("End of program");
