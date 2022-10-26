aeb0c92561fe2ff69d37c7e0fa1cd06d4c5869bc
sukhanova <verusia.s@gmail.com>
2022-10-26 12:20:27 -0500

    Added Type of Conversions coursework. Common Variables section is complete

---
+++ TypeConversionsApp/TypeConversions/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+Console.Write("Type a number between 1 and 10: ");
+
+string? userInput = Console.ReadLine();
+
+// if we try to convert wrong type ("ten" for exple) - app will crash
+// int number = int.Parse(userInput);
+
+bool isValidInt = int.TryParse(userInput, out int number);
+
+Console.WriteLine($"This is valid: {isValidInt}. The number was {number}");
+
+Console.WriteLine(number);
+Console.WriteLine(number + 10);
+
+
+double testDouble = number;
+
+// casting
+decimal testDecimal = (decimal)testDouble;
+
---
+++ TypeConversionsHomeworkApp/TypeConversionsHomework/Program.cs
@@ -0,0 +1,14 @@
+﻿/*
+Capture user's age from Console and then identify
+how old they will be in 25 years, as well as
+how old they were 25 years ago.
+
+Print that information in a Console in a natural language
+ */
+Console.Write("How old are you? ");
+string? userInput = Console.ReadLine();
+bool isInputValid = int.TryParse(userInput, out int age);
+
+Console.WriteLine($"Ok, in 25 years you will be {age +25} years old!");
+
+Console.WriteLine($"And 25 years ago you were {age - 25} years old!");
