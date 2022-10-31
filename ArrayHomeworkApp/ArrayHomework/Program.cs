ac111ab212cf2d299700ee8fabeb6be7fc77dce4
sukhanova <verusia.s@gmail.com>
2022-10-30 20:10:53 -0500

    Added arrays coursework

---
+++ ArrayHomeworkApp/ArrayHomework/Program.cs
@@ -0,0 +1,36 @@
+﻿
+
+/*
+Create an array of 3 names.
+Ask the user which number to select
+When user gives you a number, return that name.
+Make sure to check for invalid numbers
+ */
+
+using System.Threading.Tasks;
+
+string[] names = new string[] { "Kate Warne", "Annette Kellerman", "Marie Curie"};
+
+Console.Write("Choose number between 1 and 3 to reveal the name: ");
+int userInput = Convert.ToInt32(Console.ReadLine());
+if (1 <= userInput && userInput <= 3)
+{
+    switch (userInput)
+    {
+        case 1:
+            Console.WriteLine(names[0]);
+            break;
+        case 2:
+            Console.WriteLine(names[1]);
+            break;
+        case 3:
+            Console.WriteLine(names[2]);
+            break;
+    }
+}
+else
+{
+    Console.WriteLine("Invalid selection");
+}
+
+Console.WriteLine("End of program.");
---
+++ ArraysApp/Arrays/Program.cs
@@ -0,0 +1,62 @@
+﻿
+// Create an array of four elements, and add values later
+//string[] firstNames = new string[5];
+
+//firstNames[0] ="Oliver";
+//firstNames[1] = "Kyla";
+//firstNames[2] = "Tim";
+//firstNames[3] = "Jane";
+//firstNames[4] = "Jim";
+
+//Console.WriteLine($"firstName array is {firstNames.Length} names long\n");
+
+//for (int i = 0; i < firstNames.Length; i++)
+//{
+//    Console.WriteLine(firstNames[i]);
+//}
+
+
+//Console.WriteLine($"Last item in list is {firstNames[4]}");
+//firstNames[4] = "Graham";
+//Console.WriteLine($"Updated item in list is {firstNames[4]}");
+
+
+// Create an array of four elements and add values right away 
+// string[] cars = new string[4] { "Volvo", "BMW", "Ford", "Mazda" };
+
+
+// Create an array of four elements without specifying the size 
+string[] lastNames = new string[] { "Koo", "Smith", "Jones" };
+
+
+string data = "Crosby,Julia,Abby,Mike,Stephen";
+// single quote identifies a char, double quotes identifies a string
+string[] students = data.Split(',');
+
+// print array values using FOR loop:
+for (int i = 0; i < students.Length; i++)
+{
+    Console.WriteLine(students[i]);
+}
+
+
+
+// print array values using DO..WHILE loop:
+//int counter = 0;
+//do
+//{
+//    Console.WriteLine(students[counter]);
+//    counter++;
+//} while (counter < students.Length);
+
+
+// print array values using WHILE loop:
+//int counter = 0;
+//while (counter < students.Length)
+//{
+//    Console.WriteLine(students[counter]);
+//    counter++;
+//}
+
+
+
