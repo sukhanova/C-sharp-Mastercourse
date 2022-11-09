40071a18a8f3b5c6a19ac0b018595a1858ac4adf
sukhanova <verusia.s@gmail.com>
2022-11-08 18:37:03 -0600

    Guest Book App - single method

---
+++ GuestBookApp/GuestBook/GuestsData.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace GuestBook
+{
+    public class GuestsData
+    {
+
+        public static void WelcomeUser()
+        {
+            Console.WriteLine("Welcome to the Guest Book App");
+
+            Dictionary<string, int> familyInfo = new Dictionary<string, int>();
+            bool exit = false;
+            int totalGuests = 0;
+
+            while (!exit)
+            {
+                Console.Write("Type a family or type 'q' to exit: ");
+                string familyName = Console.ReadLine();
+                if (familyName.ToLower() == "q")
+                {
+                    exit = true;
+                }
+                else
+                {
+                    Console.Write($"Type numbers of guests from {familyName}'s family: ");
+                    string attendeesInput = Console.ReadLine();
+                    bool isValidInt = int.TryParse(attendeesInput, out int guestNumber);
+                    familyInfo.Add(familyName, guestNumber);
+                    totalGuests += guestNumber;
+                }
+
+                Console.Write("\nList of quest:");
+                foreach (var family in familyInfo)
+                {
+                    Console.WriteLine($"{family.Key} {family.Value}");
+                }
+
+                Console.WriteLine($"\nSo far there are {totalGuests} guests");
+            }
+
+        }
+    }
+}
+
---
+++ GuestBookApp/GuestBook/Program.cs
@@ -0,0 +1,43 @@
+﻿
+
+/*
+ Build a Console Guest Book.
+Ask user for their name and how many are ion their party.
+Keep asking until there are no families coming.
+Keep track of how many people are at the party.
+At the end, print out the guest list and total number of guests.
+
+Use methods
+
+Input: Mishra family, 3 guests
+Output: Mishra family, Stall family, 8 gusts total.
+
+
+1. Welcome the user (Intro the app name as well)
+
+Ask user for their name
+Store the name
+
+
+2. Ask user for number in party
+
+<convert string to number>
+Add the number to the total
+
+
+3. Check to see if more guests are coming
+
+Loop back to the top until no more guests
+
+
+4. Loop over the names list and print them
+
+
+
+5. Print the total guest count
+ 
+ */
+
+using GuestBook;
+
+GuestsData.WelcomeUser();
